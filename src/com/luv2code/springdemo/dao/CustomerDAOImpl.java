10d0b87086bf375a01f6ac4ff850e0d99a7e4a4d
Jesus Zuniga <jesus.zuniga@macys.com>
2019-07-29 12:18:47 -0400

    Adding Update method with Hibernate

--- src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
+++ src/com/luv2code/springdemo/dao/CustomerDAOImpl.java
@@ -40,7 +40,20 @@ public class CustomerDAOImpl implements CustomerDAO {
         Session currentSession = sessionFactory.getCurrentSession();
 
         // save the customer... finally LOL
-        currentSession.save(theCustomer);
+        // hibernate will check if the primary key/id is empty, so it will insert or update
+        currentSession.saveOrUpdate(theCustomer);
 
+    }
+
+    @Override
+    public Customer getCustomer(int theId) {
+
+        // get the current hibernate session
+        Session currentSession = sessionFactory.getCurrentSession();
+
+        // now retrieve/read from database using the primary key
+        Customer theCustomer = currentSession.get(Customer.class, theId);
+
+        return theCustomer;
     }
 }
